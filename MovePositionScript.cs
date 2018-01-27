0b1239ec3e0882859d21c98fda289e974e68e1d4
josette-seitz <josette.seitz@tma1.com>
2018-01-27 15:38:44 -0500

    Add files via upload

---
+++ MovePositionScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePositionScript : MonoBehaviour {
+    public Transform[] pathPoints;
+    public float moveSpeed = 12;
+    private int currPath;
+    Color[] randColor = new Color[6];
+
+    private void Start() {
+        randColor[0] = Color.red;
+        randColor[1] = Color.blue;
+        randColor[2] = Color.green;
+        randColor[3] = Color.yellow;
+        randColor[4] = Color.magenta;
+        randColor[5] = Color.cyan;
+    }
+
+    void Update () {
+        if (transform.position != pathPoints[currPath].position)
+        {
+            Vector3 moveToNextPoint = Vector3.MoveTowards(transform.position, pathPoints[currPath].position, moveSpeed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(moveToNextPoint);
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = randColor[Random.Range(0, randColor.Length)];
+            currPath++;
+            if (currPath.Equals(pathPoints.Length))
+            {
+                currPath = 0;
+            }
+        }
+    }
+}
---
+++ MoveToScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToScript : MonoBehaviour {
+    public Transform[] pathPoints;
+    public float moveSpeed = 12;
+    private int currPath = 0;
+    Color[] randColor = new Color[6];
+
+    void Start () {
+        randColor[0] = Color.red;
+        randColor[1] = Color.blue;
+        randColor[2] = Color.green;
+        randColor[3] = Color.yellow;
+        randColor[4] = Color.magenta;
+        randColor[5] = Color.cyan;
+    }
+	
+	void Update () {
+            iTween.MoveTo(gameObject, iTween.Hash("position", pathPoints[currPath].position, 
+                "speed", moveSpeed, "easetype", "linear", "oncomplete", "PathOfQueen"));
+	}
+
+    void PathOfQueen()
+    {
+        GetComponent<Renderer>().material.color = randColor[Random.Range(0, randColor.Length)];
+        currPath++;
+        if (currPath.Equals(pathPoints.Length))
+        {
+            currPath = 0;
+        }
+    }
+}
+
+//This will also work w/ a curve path
+//private float moveSpeed = 0.12f;
+//private float currPath = 0;
+//if (currPath > 1.0)
+//{
+//    currPath = 0;
+//}
+//currPath += moveSpeed * Time.deltaTime;
+//iTween.PutOnPath(transform, pathPoints, currPath);
